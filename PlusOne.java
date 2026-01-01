e5d0a0e7577b4c90647c9846480db65f274dff7e
Adarsh Gupta <adarshgupta664@gmail.com>
2026-01-01 23:08:32 +0530

    Plus One
    We are given a number in the form of an integer array, where:
    Each element represents one digit
    Digits are stored from left to right
    The number has no leading zero
    Our task is to add 1 to this number and return the updated digits array.

---
+++ PlusOne.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] digits1 = {1, 2, 3};
+        int[] digits2 = {4, 3, 2, 1};
+        int[] digits3 = {9, 9, 9};
+
+        System.out.println(Arrays.toString(plusOne(digits1)));
+        System.out.println(Arrays.toString(plusOne(digits2)));
+        System.out.println(Arrays.toString(plusOne(digits3)));
+    }
+}
