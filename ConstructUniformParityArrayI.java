bbea75a010f9f9b732ee2375247d5b9e536538ca
Adarsh Gupta <adarshgupta664@gmail.com>
2026-09-02 12:03:04 +0530

    Construct Uniform Parity Array I
    You are given an array nums1 of n distinct integers.
    You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
    For each index i, you must choose exactly one of the following (in any order):

---
+++ ConstructUniformParityArrayI.java
@@ -0,0 +1,11 @@
+class ConstructUniformParityArrayI {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+    public static void main(String[] args) {
+        ConstructUniformParityArrayI obj = new ConstructUniformParityArrayI();
+        int[] nums1 = {2,10,7,5,4,1,8,6};
+        boolean result = obj.uniformArray(nums1);
+        System.out.println(result);
+    }
+}
